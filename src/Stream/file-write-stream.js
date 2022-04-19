24ca7e9a7596bebc4e631d698f589a06769c1363
KingComedy <1455445725@qq.com>
2022-04-19 23:16:56 +0800

    'file-write'

---
+++ src/Stream/file-write-stream.js
@@ -0,0 +1,35 @@
+/* 
+  文件可写流
+*/
+const fs = require('fs')
+const ws = fs.createWriteStream('./test.txt', {
+  flags: 'w',
+  encoding: 'utf-8',
+  fd: null,
+  mode: 438,
+  autoClose: true,
+  start: 0,
+  highWaterMark: 4 // 每次写多少字节到缓存区
+})
+
+let buf = Buffer.from('write')
+// 只能是string 或者buffer类型
+// ws.write(buf, () => {
+//   console.log('write success')
+// })
+
+ws.on('open', (fd) => {
+  console.log('文件打开了')
+})
+
+ws.on('end', () => {
+  console.log('写操作完成')
+})
+ws.write(buf)
+ws.end('end111', () => {
+  console.log('end')
+})
+
+ws.on('error', (err) => {
+  console.log(err)
+})
